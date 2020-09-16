5fa1e20202d1b100ed7b0215f92243b44878921f
Christian Påbøl Jacobsen <cpcp800@auroragames.dk>
2020-09-17 00:01:12 +0200

    funstuff

--- w2/w2-code-handin/cuda/spMV-Mul-kernels.cu.h
+++ w2/w2-code-handin/cuda/spMV-Mul-kernels.cu.h
@@ -26,13 +26,18 @@ __global__ void
 mult_pairs(int* mat_inds, float* mat_vals, float* vct, int tot_size, float* tmp_pairs) {
     // ... fill in your implementation here ...
     uint32_t gid = blockIdx.x * blockDim.x + threadIdx.x;
-    if (gid)
-        printf("%d\n", tot_size);
+    if (gid < tot_size){
+        tmp_pairs[gid] = mat_vals[gid] * vct[mat_inds[gid]];
+    }
 }
 
 __global__ void
 select_last_in_sgm(int mat_rows, int* mat_shp_sc_d, float* tmp_scan, float* res_vct_d) {
     // ... fill in your implementation here ...
+    uint32_t gid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (gid < mat_rows){
+        res_vct_d[gid] = tmp_scan[mat_shp_sc_d[gid] - 1]
+    }
 }
 
 #endif
