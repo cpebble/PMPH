a519fd466b5a0d61bbec096bb59d0388752123c0
Christian Påbøl Jacobsen <cpcp800@auroragames.dk>
2020-09-16 23:41:33 +0200

    explore

--- w2/w2-code-handin/cuda/spMV-Mul-kernels.cu.h
+++ w2/w2-code-handin/cuda/spMV-Mul-kernels.cu.h
@@ -13,7 +13,7 @@ __global__ void
 mkFlags(int mat_rows, int* mat_shp_sc_d, char* flags_d) {
     uint32_t gid = blockIdx.x * blockDim.x + threadIdx.x;
     if (gid == 0){
-        printf("%d\n", mat_shp_sc_d[1]);
+        printf("mat_rows: %d, %d\n", mat_rows, mat_shp_sc_d[2]);
     }
     // ... fill in your implementation here ...
 }
