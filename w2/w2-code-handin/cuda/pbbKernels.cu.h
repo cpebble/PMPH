d6109b2c73f43e5caba70e51f8b919d8830d2f59
Christian Påbøl Jacobsen <cpcp800@auroragames.dk>
2020-09-16 16:10:40 +0200

    This is totally a guess

--- w2/w2-code-handin/cuda/pbbKernels.cu.h
+++ w2/w2-code-handin/cuda/pbbKernels.cu.h
@@ -435,10 +435,12 @@ copyFromGlb2ShrMem( const uint32_t glb_offs
 ) {
     #pragma unroll
     for(uint32_t i=0; i<CHUNK; i++) {
-        uint32_t loc_ind = threadIdx.x*CHUNK + i;
+        uint32_t loc_ind = threadIdx.x+CHUNK * i;
         uint32_t glb_ind = glb_offs + loc_ind;
         T elm = ne;
-        if(glb_ind < N) { elm = d_inp[glb_ind]; }
+        if(glb_ind < N) { 
+            elm = d_inp[glb_ind];
+        }
         shmem_inp[loc_ind] = elm;
     }
     __syncthreads(); // leave this here at the end!
